91fa571840103fa0ce7ef440f6bd7bf5f232f224
ProGrad Junior <70092746+ProGradJr@users.noreply.github.com>
2021-08-31 16:11:14 +0530

    Update tts.py

--- tts.py
+++ tts.py
@@ -3,27 +3,19 @@ import pyttsx3
 
 engine = pyttsx3.init()
 
-class Widget():
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.title('TTS')
-        self.root.configure(background="pink")
-        self.root.resizable(0,0)
-        self.label = tk.Label(self.root, text="Type Something", bg="pink", font="Arial 30 bold")
-        self.label.pack()
-        self.textbox = tk.Entry(self.root, width=35, font="Arial 25")
-        self.textbox.pack()
-        self.button = tk.Button(self.root, text= "SPEAK", bg="blue", font="Arial 25 bold", command=self.clicked)
-        self.button.pack()
-        self.root.mainloop()
+root = tk.Tk()
+root.title('TTS')
+root.configure(background="pink")
+root.resizable(0,0)
+label = tk.Label(root, text="Type Something", bg="pink", font="Arial 30 bold")
+label.pack()
+textbox = tk.Entry(root, width=35, font="Arial 25")
+textbox.pack()
 
-    def clicked(self):
-        text = self.textbox.get()
-        self.speak(text)
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
 
-    def speak(self,text):
-        engine.say(text)
-        engine.runAndWait()
-
-if __name__ == "__main__":
-    temp = Widget()
+button = tk.Button(root, text= "SPEAK", bg="blue", font="Arial 25 bold", command=lambda:speak(textbox.get()))
+button.pack()
+root.mainloop()
